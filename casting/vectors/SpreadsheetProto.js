a1ff97f85555e7c0dc91dbc7979e447a9f753c8f
gabywalton <ggw12@georgetown.edu>
2019-07-29 12:32:26 +0200

    Update SpreadsheetProto.js

--- casting/vectors/SpreadsheetProto.js
+++ casting/vectors/SpreadsheetProto.js
@@ -15,7 +15,7 @@ function main() {
   //var Steps = importResearchSteps();
   var Steps = importSteps();
   
-  // MAKING ALL THE NECESSARY TABS
+  // MAKING ALL THE NECESSARY TABS and filling them
   // 1. the governance tabs
   populate(file, Indicators.governance, Verizon, Steps);
 
@@ -33,6 +33,7 @@ function main() {
 
 
 
+// functions to convert column numbers to letters and vice versa
 function columnToLetter(column)
 {
   var temp, letter = '';
@@ -67,10 +68,11 @@ function populate(file, indicatortype, Verizon, Steps) {
     
     var subtype =1;
   
-    if(indicatortype.hasComponents==true) { subtype=indicatortype.components.length;}
+    if(indicatortype.hasComponents==true) { subtype=indicatortype.components.length;} // if indicator has components add them
   
     activeRow = header(spread, indicatortype, Verizon, activeRow, file, subtype); // sets up header
     
+    // setting up all the steps for all the indicators
      for( var j=0; j<Steps.researchSteps.length; j++) {
       for( var k=0; k<Steps.researchSteps[j].elements.length; k++) {
         
@@ -78,23 +80,27 @@ function populate(file, indicatortype, Verizon, Steps) {
           activeRow =stepHeader(spread, indicatortype.indicators[i], Verizon, activeRow, file, Steps.researchSteps[j], k, subtype);
         }
         
-        if(Steps.researchSteps[j].elements[k].type == "elementDropDown") {
+        else if(Steps.researchSteps[j].elements[k].type == "elementDropDown") {
           activeRow = elementDropDown(spread, indicatortype.indicators[i], Verizon, activeRow, file, Steps.researchSteps[j], k, subtype, indicatortype);
         }
         
-       if(Steps.researchSteps[j].elements[k].type == "comments") {
+        else if(Steps.researchSteps[j].elements[k].type == "miniElementDropDown") {
+          activeRow = miniElementDropDown(spread, indicatortype.indicators[i], Verizon, activeRow, file, Steps.researchSteps[j], k, subtype, indicatortype);
+        }
+        
+       else if(Steps.researchSteps[j].elements[k].type == "comments") {
           activeRow = comments(spread, indicatortype.indicators[i], Verizon, activeRow, file, Steps.researchSteps[j], k, subtype, indicatortype);
         }
         
-        if(Steps.researchSteps[j].elements[k].type == "sources") {
+        else if(Steps.researchSteps[j].elements[k].type == "sources") {
           activeRow = sourcesStep(spread, indicatortype.indicators[i], Verizon, activeRow, file, Steps.researchSteps[j], k, subtype, indicatortype);
         }
         
-        if(Steps.researchSteps[j].elements[k].type == "miniheader") {
+        else if(Steps.researchSteps[j].elements[k].type == "miniheader") {
           activeRow = miniheader(Steps.researchSteps[j], k, activeRow, spread);
         }
         
-        if(Steps.researchSteps[j].elements[k].type == "comparison") {
+        else if(Steps.researchSteps[j].elements[k].type == "comparison") {
           activeRow = comparison(spread, indicatortype.indicators[i], Verizon, activeRow, file, Steps.researchSteps[j], k, subtype, indicatortype);
         }
       }
@@ -109,7 +115,7 @@ function comparison(spread, indicator, Verizon, activeRow, file, step, m, subtyp
   // sets up column with discription
   for(var j=0; j<indicator.elements.length; j++) {
     var activeCol = 1;
-    for(var k=0; k<(Verizon.numberOfServices+3); k++) { // (((Verizon.numberOfServices+2)*subtype)+1)
+    for(var k=0; k<(Verizon.numberOfServices+3); k++) {
       if(k==0) {
          var cell =spread.getRange(activeRow+j, activeCol);
          cell.setValue(step.elements[m].label + indicator.elements[j].labelShort);
@@ -335,6 +341,72 @@ function stepHeader(spread, indicator, Verizon, activeRow, file, step, k, subtyp
   
 }
 
+function miniElementDropDown(spread, indicator, Verizon, activeRow, file, step, m, subtype, indicatortype) {
+  var rule = SpreadsheetApp.newDataValidation().requireValueInList(step.elements[m].dropdown).build();
+
+  var activeCol = 1;
+    for(var k=0; k<(Verizon.numberOfServices+3); k++) { // (((Verizon.numberOfServices+2)*subtype)+1)
+      if(k==0) {
+         var cell =spread.getRange(activeRow, activeCol);
+         cell.setValue(step.elements[m].label);
+         cell.setBackgroundRGB(step.c1, step.c2, step.c3);
+        activeCol=activeCol+1;
+        }
+      
+      // names the cells into which answers will be put
+      else {
+        if(k==1) {
+          // overall company
+          for(var g=0; g<subtype;g++) {
+                    var thisCell = spread.getRange(activeRow, activeCol);
+          var trial = ('RDR2019DC'+Verizon.id+step.labelShort);
+            if(subtype!=1) { trial=trial+indicatortype.components[g].labelShort;
+            }
+          trial=trial.toString();
+          file.setNamedRange(trial,thisCell); // names cells
+        thisCell.setDataValidation(rule); // creates dropdown list
+        thisCell.setValue('not selected'); // sets default for drop down list
+        thisCell.setFontWeight('bold'); // bolds the answers
+          activeCol=activeCol+1;
+          }
+        }
+        
+        else if (k==2) {
+          for(var g=0; g<subtype;g++) {
+                    var thisCell = spread.getRange(activeRow, activeCol);
+          var thisCell = spread.getRange(activeRow, activeCol);
+          var trial = ('RDR2019DC'+Verizon.id+'opCom'+step.labelShort);
+            if(subtype!=1) { trial=trial+indicatortype.components[g].labelShort;
+            }
+          trial=trial.toString();
+          file.setNamedRange(trial,thisCell); // names cells
+        thisCell.setDataValidation(rule); // creates dropdown list
+        thisCell.setValue('not selected'); // sets default for drop down list
+        thisCell.setFontWeight('bold'); // bolds the answers
+          activeCol=activeCol+1;
+          }
+        }
+        
+        else {
+          for(var g=0; g<subtype;g++) {
+                    var thisCell = spread.getRange(activeRow, activeCol);
+          var trial = ('RDR2019DC'+Verizon.services[k-3].id+step.labelShort);
+          if(subtype!=1) { trial=trial+indicatortype.components[g].labelShort;}
+          trial=trial.toString();
+          file.setNamedRange(trial,thisCell); // names cells
+          thisCell.setDataValidation(rule); // creates dropdown list
+            thisCell.setValue('not selected'); // sets default for drop down list
+            thisCell.setFontWeight('bold'); // bolds the answers
+          activeCol=activeCol+1;
+          }
+        }   
+      }
+    }
+  
+  activeRow = activeRow+1;
+  return activeRow;
+}
+
 
 function elementDropDown(spread, indicator, Verizon, activeRow, file, step, m, subtype, indicatortype) {
   
@@ -521,162 +593,6 @@ function sourcesStep(spread, indicator, Verizon, activeRow, file, step, m, subty
   return activeRow;
 }
 
-// --------------------------------------Step 1 Function------------------------------------
-
-function step1(spread, indicator, Verizon, activeRow, activeCol, file) {
-  
-  // will be used later in dropdown list:
-    var rule = SpreadsheetApp.newDataValidation().requireValueInList(['not selected', 'yes','partial','no','no disclosure found','N/A']).build();
-
-  // --------------------------Setting up step and your name row--------------------------
-  activeCol=1;
-  activeRow=activeRow+1;
-  for(var i=0; i<(Verizon.numberOfServices+3);i++) { // +3 because set up collumn, overall, and opCom
-    var cell = spread.getRange(activeRow,i+1);
-    if(i==0) {
-      cell.setBackgroundRGB(223, 135, 212);
-      var text ='Step 1: Data Collection';
-      cell.setValue(text);
-      cell.setFontWeight('bold');
-    }
-    
-    else {
-      cell.setValue('Your Name');
-    }
-    
-  }
-  
-    // --------------------------Setting up subindicator rows--------------------------
-
-  // moving on to actual indicators
-  // sets up column with discription
-  activeCol=1;
-  activeRow=activeRow+1;
-  for(var j=0; j<indicator.elements.length; j++) {
-    for(var k=0; k<(Verizon.numberOfServices+3); k++) {
-      if(k==0) {
-        var cell =spread.getRange(activeRow+j, k+1);
-        cell.setValue('Element ' + indicator.elements[j].labelShort);
-        cell.setBackgroundRGB(223, 135, 212);
-        
-      }
-      
-      // names the cells into which answers will be put
-      else {
-        var thisCell = spread.getRange(activeRow+j, k+1);
-        if(k==1) {
-          // overall company
-          var trial = ('RDR2019DC'+Verizon.id+'S01'+indicator.elements[j].labelShort);
-          trial=trial.toString();
-          file.setNamedRange(trial,thisCell); // names cells
-        }
-        
-        else if (k==2) {
-          // opCom
-          var trial = ('RDR2019DC'+Verizon.id+'opCom'+'S01'+indicator.elements[j].labelShort);
-          trial=trial.toString();
-          file.setNamedRange(trial,thisCell); // names cells
-        }
-        
-        else {
-          // subservices
-          var trial = ('RDR2019DC'+Verizon.services[k-3].id+'S01'+indicator.elements[j].labelShort);
-          trial=trial.toString();
-          file.setNamedRange(trial,thisCell);
-        }
-        thisCell.setDataValidation(rule); // creates dropdown list
-        thisCell.setValue('not selected'); // sets default for drop down list
-        thisCell.setFontWeight('bold'); // bolds the answers
-
-        
-      }
-      //Logger.log(k);
-    }
-  }
-  
-  
-  // -----------------------------Comments row--------------------------------
-  activeCol=1;
-  activeRow=activeRow+indicator.elements.length;
-  for(var i =0;i<indicator.elements.length;i++) {spread.setRowHeight(activeRow+i,50);} // increases height of row
-  for(var j=0; j<indicator.elements.length; j++) {
-    for(var k=0; k<(Verizon.numberOfServices+3); k++) {
-     if(k==0) {
-       // first column label
-       var cell = spread.getRange(activeRow+j, k+1);
-       cell.setValue('Comments for Element '+indicator.elements[j].labelShort+' (explain score)');
-       cell.setBackgroundRGB(223, 135, 212); // colors cell
-
-     }
-      
-     else {
-       // names the cells
-        var thisCell = spread.getRange(activeRow+j, k+1);
-       thisCell.setWrap(true);
-        if(k==1) {
-          var trial = ('RDR2019DC'+Verizon.id+'S01'+indicator.elements[j].labelShort+'Comments');
-          trial=trial.toString();
-          file.setNamedRange(trial,thisCell);
-
-        }
-       else if (k==2) {
-          // opCom
-          var trial = ('RDR2019DC'+Verizon.id+'opCom'+'S01'+indicator.elements[j].labelShort+'Comments');
-          trial=trial.toString();
-          file.setNamedRange(trial,thisCell); // names cells
-        }
-        
-        else {
-          var trial = ('RDR2019DC'+Verizon.services[k-3].id+'S01'+indicator.elements[j].labelShort+'Comments');
-          trial=trial.toString();
-          file.setNamedRange(trial,thisCell);
-        }
-
-        
-      }
-    }
-  }
-  
-  //-----------------------------Sources row------------------------------------
-  activeRow=activeRow+indicator.elements.length;
-    for(var k=0; k<(Verizon.numberOfServices+3); k++) {
-     if(k==0) {
-       // first column description
-       var cell =spread.getRange(activeRow,activeCol);
-       cell.setValue('Sources (reference, specific page, section, etc.)');
-       cell.setBackgroundRGB(223, 135, 212);
-
-     }
-     else {
-        var thisCell = spread.getRange(activeRow, k+1);
-        thisCell.setWrap(true); // sets wrap so that text doesn't bleed off the side
-
-        if(k==1) {
-          // main company
-          var trial = ('RDR2019DC'+Verizon.id+'S01'+indicator.labelShort+'Sources');
-          trial=trial.toString();
-          file.setNamedRange(trial,thisCell);
-        }
-       
-       else if (k==2) {
-          // opCom
-          var trial = ('RDR2019DC'+Verizon.id+'opCom'+'S01'+indicator.labelShort+'Sources');
-          trial=trial.toString();
-          file.setNamedRange(trial,thisCell); // names cells
-        }
-        
-        else {
-          // services
-          var trial = ('RDR2019DC'+Verizon.services[k-3].id+'S01'+indicator.labelShort+'Sources');
-          trial=trial.toString();
-          file.setNamedRange(trial,thisCell);
-        }
-        
-        
-      }
-    }
-  return activeRow;
-}
 
 
 
@@ -781,6 +697,65 @@ function importSteps() {
           "nameLabel": "Comments"
         }
       ]
+    },
+    {
+      "label": "Step 2: Review",
+      "labelShort": "S02",
+      "c1": 186,
+      "c2": 136,
+      "c3": 177,
+      "elements": [
+        {
+          "type": "header",
+          "filler": "Your Name"
+        },
+        {
+          "type": "miniElementDropDown",
+          "label": "Do you agree with the answer(s) in Step 1?",
+          "dropdown": [
+            "not selected",
+            "yes",
+            "no"
+          ]
+        },
+        {
+          "type": "elementDropDown",
+          "label": "If 'no': suggested answer for ",
+          "dropdown": [
+            "not selected",
+            "yes",
+            "partial",
+            "no",
+            "no disclosure found",
+            "N/A"
+          ]
+        },
+        {
+          "type": "comments",
+          "label": "Comments for ",
+          "label2": " (required if 'no', optional if 'yes')",
+          "nameLabel": "Comments"
+        },
+        {
+          "type": "miniheader",
+          "label": "Do you agree with the year-on-year analysis in Step 1.5?"
+        },
+        {
+          "type": "elementDropDown",
+          "label": "In Step 1.5 for ",
+          "dropdown": [
+            "not selected",
+            "yes",
+            "no"
+          ]
+        },
+        {
+          "type": "comments",
+          "label": "If no, comments on ",
+          "label2": " If no, comments on ",
+          "nameLabel": "Comments2"
+        }
+        ]
     },
     {
       "label": "Step 3: Score Consensus",
@@ -939,15 +914,48 @@ function importSteps() {
     {
       "label": "Step 5: Company Feedback",
       "labelShort": "S05",
-      "c1": 50,
-      "c2": 168,
-      "c3": 98,
+      "c1": "143",
+      "c2": "237",
+      "c3": "88",
       "elements": [
         {
           "type": "header",
           "filler": "Your Name"
+        },
+        {
+          "type": "miniElementDropDown",
+          "label": "Does company feedback merit a change??",
+          "dropdown": [
+            "not selected",
+            "yes",
+            "no"
+          ]
+        },
+        {
+          "type": "elementDropDown",
+          "label": "If 'yes': suggested answer for ",
+          "dropdown": [
+            "not selected",
+            "yes",
+            "partial",
+            "no",
+            "no disclosure found",
+            "N/A"
+          ]
+        },
+        {
+          "type": "comments",
+          "label": "Comments for ",
+          "label2": " (required if 'yes', optional if 'no')",
+          "nameLabel": "Comments"
+        },
+        {
+          "type": "sources",
+          "label": "Sources (reference, specific page, section, etc.)",
+          "nameLabel": "Sources"
         }
-      ]
+        
+        ]
     },
     {
       "label": "Step 6: Second Horizontal Review",
