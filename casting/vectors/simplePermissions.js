c7ca9cbe1f42fbabb48795b729e609fee5c72446
fubits <ilja_s@pm.me>
2019-08-08 16:18:47 +0200

    Update simplePermissions.js

--- casting/vectors/simplePermissions.js
+++ casting/vectors/simplePermissions.js
@@ -19,45 +19,82 @@ var permissionRangesJSON = {
   }
 }
 
-function clearAllStepWise() {
-  var ss = SpreadsheetApp.getActive();
-  var protections = ss.getProtections(SpreadsheetApp.ProtectionType.RANGE);
-  for (var i = 0; i < protections.length; i++) {
-    var protection = protections[i];
-    if (protection.canEdit()) {
-      protection.remove();
+var dummy = 1213123123;
+var spreadsheetName = "CompanyObjPrototypeG5Permissions";
+
+function clearAll() {
+  var ss = connectToSpreadsheetByName(spreadsheetName);
+  Logger.log(ss.getName());
+  var protectionsRanges = ss.getProtections(SpreadsheetApp.ProtectionType.RANGE);
+  for (var i = 0; i < protectionsRanges.length; i++) {
+    var protectionRange = protectionsRanges[i];
+    if (protectionRange.canEdit()) {
+      protectionRange.remove();
+    }
+  }
+  var protectionsSheets = ss.getProtections(SpreadsheetApp.ProtectionType.SHEET);
+  for (var i = 0; i < protectionsSheets.length; i++) {
+    var protectionSheet = protectionsSheets[i];
+    if (protectionSheet.canEdit()) {
+      protectionSheet.remove();
     }
   }
 }
 
-// just for testing
+// Main Caller
 
-function connect() {
-  var Spreadsheet = SpreadsheetApp.getActive();
-  var Sheet = Spreadsheet.getSheetByName("G5");
-  Logger.log(Sheet.getLastRow())
-}
-
-// main Logic; proof-of-concept
-
-// PROTECT step-wise caller
-
+//this works
 function mainStepWiseProtect() {
-  Logger.log(permissionRangesJSON.g5ranges.step1)
-  var Spreadsheet = SpreadsheetApp.getActive();
-  setStepProtection(Spreadsheet, "G5", permissionRangesJSON.g5ranges.step1);
-  setStepProtection(Spreadsheet, "G5", permissionRangesJSON.g5ranges.step1_5);
-  setStepProtection(Spreadsheet, "G5", permissionRangesJSON.g5ranges.step2);
-  setStepProtection(Spreadsheet, "G5", permissionRangesJSON.g5ranges.step3);
+  StepWiseProtect(spreadsheetName)
 }
 
-// function
+// this is not operational yet
+function mainStepWiseUnProtect() {
+  StepWiseUnProtect(spreadsheetName)
+}
 
-function setStepProtection(Spreadsheet, Indicator, Step) { // params step, indicator, editor
-  var currentSheet = Spreadsheet.getSheetByName(Indicator)
+// Universal Method: Connect to Spreadsheet File and return Obj as open Spreadsheet
+// This works
+function connectToSpreadsheetByName(spreadsheetName) {
+  var Spreadsheets = DriveApp.getFilesByName(spreadsheetName);
+  if (!Spreadsheets.hasNext()) {
+    Logger.log("Nothing");
+    return null;
+  } else {
+    // while (Spreadsheet.hasNext()) {
+    // Nope. Only do for first Spreadsheet element
+    var thisSpreadsheet = Spreadsheets.next();
+    Logger.log("locally connected to: " + thisSpreadsheet.getName());
+    Logger.log("it's a " + thisSpreadsheet.getMimeType());
+    return SpreadsheetApp.open(thisSpreadsheet);
+    // }
+  }
+}
+
+// step-wise caller
+// turn into for (i in ranges) DO {}
+// this works
+function StepWiseProtect(spreadsheetName) {
+  Logger.log("ranges: " + permissionRangesJSON.g5ranges.step1);
+  Logger.log("to be connected to: " + spreadsheetName);
+  // var Spreadsheet = SpreadsheetApp.getActive();
+  var thisSpreadsheet = connectToSpreadsheetByName(spreadsheetName);
+  Logger.log("remote connected to: " + thisSpreadsheet.getName());
+  setStepProtection(thisSpreadsheet, "G5", permissionRangesJSON.g5ranges.step1);
+  setStepProtection(thisSpreadsheet, "G5", permissionRangesJSON.g5ranges.step1_5);
+  setStepProtection(thisSpreadsheet, "G5", permissionRangesJSON.g5ranges.step2);
+  setStepProtection(thisSpreadsheet, "G5", permissionRangesJSON.g5ranges.step3);
+}
+
+// atomic single step protection function
+// this works
+function setStepProtection(thisSpreadsheet, Indicator, Step) { // params step, indicator, editor
+  Logger.log("received " + thisSpreadsheet);
+  // Logger.log("received " + thisSpreadsheet.getCurrentCell());
+  var currentSheet = thisSpreadsheet.getSheetByName(Indicator);
   var me = Session.getEffectiveUser();
-  var editors = Spreadsheet.getEditors();
-  Logger.log(editors);
+  var editors = thisSpreadsheet.getEditors();
+  // Logger.log(editors);
 
   var range = currentSheet.getRange(Step.range);
   if (range.canEdit()) {
@@ -68,7 +105,7 @@ function setStepProtection(Spreadsheet, Indicator, Step) { // params step, indic
     if (protection.canDomainEdit()) {
       protection.setDomainEdit(false);
     }
-    Logger.log("Step 1 now prtected")
+    Logger.log("Step " + Step.step + " now prtected")
   } else {
     Logger.log("Step 1 is already protected")
   }
@@ -100,15 +137,27 @@ function removeStepProtection(Spreadsheet, Indicator, Step) { // params step, in
 
 }
 
-var sheetName = "G5";
 
-function wholeSheetProtect(sheetName){
- var ss = SpreadsheetApp.getActive()
- var sheet = ss.getSheetByName(sheetName);
- var protection = sheet.protect().setDescription('G5 Protected');
+// SHeet-level permissions
+// works
+
+var indicatorString = "G5";
+
+function mainWholeSheetProtect() {
+  wholeSheetProtect(indicatorString);
 }
 
-function wholeSheetUnProtect(sheetName){
+function mainWholeSheetUnProtect() {
+  wholeSheetUnProtect(indicatorString);
+}
+
+function wholeSheetProtect(sheetName) {
+  var ss = SpreadsheetApp.getActive()
+  var sheet = ss.getSheetByName(sheetName);
+  var protection = sheet.protect().setDescription('G5 Protected');
+}
+
+function wholeSheetUnProtect(sheetName) {
   var ss = SpreadsheetApp.getActive();
   var protections = ss.getProtections(SpreadsheetApp.ProtectionType.SHEET);
   for (var i = 0; i < protections.length; i++) {
@@ -120,28 +169,20 @@ function wholeSheetUnProtect(sheetName){
 }
 
 
-function mainWholeSheetProtect () {
-  wholeSheetProtect("G5");
-}
-
-function mainWholeSheetUnProtect () {
-  wholeSheetUnProtect("G5");
-}
-
-function iteratorDummy(){
+// saved for later; not in use right now 
+function iteratorDummy() {
   //set permissions
-    var ss = SpreadsheetApp.getActiveSpreadsheet();
-    var ranges = ["A1:K","F1:F","H1:H","Y1:Y","AD1:AF"];
-    
-    for (var j in ranges){
-      var protection = active.getRange(ranges[j]).protect();
-  
-      var me = Session.getEffectiveUser();
-      protection.addEditor(me);
-      protection.removeEditors(protection.getEditors());
-      if (protection.canDomainEdit()) {
-        protection.setDomainEdit(false);
-      }
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var ranges = ["A1:K", "F1:F", "H1:H", "Y1:Y", "AD1:AF"];
+
+  for (var j in ranges) {
+    var protection = active.getRange(ranges[j]).protect();
+
+    var me = Session.getEffectiveUser();
+    protection.addEditor(me);
+    protection.removeEditors(protection.getEditors());
+    if (protection.canDomainEdit()) {
+      protection.setDomainEdit(false);
     }
   }
-  
+}
