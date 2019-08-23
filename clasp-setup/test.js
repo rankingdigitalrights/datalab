e142e52e9d52970e28a98cffb0ec5054b1ef0b3e
fubits <ilja_s@pm.me>
2019-08-23 11:53:12 +0200

    minimal clasp example

---
+++ clasp-setup/test.js
@@ -0,0 +1,5 @@
+function runTest() {
+    Logger.log("Hello, world! This is clasp + vscode");
+    Logger.log("This has been edited online and then clasp pull(ed)");
+    Logger.log("This has been edited offline and then clasp push(ed)");
+}
