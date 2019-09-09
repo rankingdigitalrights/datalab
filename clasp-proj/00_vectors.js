3674cbd761a0250a131b4f6976dea257de64b11d
fubits <ilja_s@pm.me>
2019-09-09 19:51:14 +0200

    JSONs re-edited, consolidated in companies.json and JS

--- clasp-proj/00_vectors.js
+++ clasp-proj/00_vectors.js
@@ -1,97 +1,234 @@
+// (for company in companiesVector)
+// var thisCompany = companiesVector[company]
+
 var companiesVector = {
   "collection": "companies",
   "indexYear": 2019,
   "indexType": "RDR",
   "companies": [
-    {
-      "id": "tMT1",
-      "label": {
-        "current": "MTN",
-        "legacy": [null]
-      },
-      "type": "telecom",
-      "groupLabel": "MTN (Group)",
-      "opCom": true,
-      "opComLabel": "MTN South Africa (Operating Company)",
-      "prevScored": true,
-      "firstIndex": 2015,
-      "urlPreviousYearResults": "https://docs.google.com/spreadsheets/d/1mOB5yheHPKrPAb_HTpJA27IxligiggJHM4MCv1JGh9Y",
-      "tabPrevYearsOutcome": "MTNOutcome",
-      "urlCurrentDataCollectionSheet": null,
-      "urlCurrentCompanyScoringSheet": null,
-      "numberOfServices": 3,
-      "services": [{
-        "id": "bbMT1",
-        "type": "broadband",
-        "label": {
-          "current": "Fixed-line broadband",
-          "legacy": null
-        }
+
+      {
+          "id": "iBA1",
+          "label": {
+              "current": "Baidu",
+              "legacy": null
+          },
+          "type": "internet",
+          "groupLabel": "Baidu (Group)",
+          "opCom": false,
+          "opComLabel": null,
+          "prevScored": true,
+          "firstIndex": 2015,
+          "urlPreviousYearResults": "https://docs.google.com/spreadsheets/d/1bAVVenbXBzWLTwYahqp-8DbHMx5S2YS7a4VhTe8RjFE",
+          "tabPrevYearsOutcome": "BaiduOutcome",
+          "urlCurrentDataCollectionSheet": null,
+          "urlCurrentCompanyScoringSheet": null,
+          "numberOfServices": 3,
+          "services": [{
+                  "id": "seBA1",
+                  "type": "search",
+                  "label": {
+                      "current": "Baidu Search",
+                      "legacy": null
+                  }
+              },
+              {
+                  "id": "clBA2",
+                  "type": "cloud",
+                  "label": {
+                      "current": "Baidu Cloud",
+                      "legacy": null
+                  }
+              },
+              {
+                  "id": "mvBA3",
+                  "type": "messagingVoip",
+                  "label": {
+                      "current": "Baidu PostBar",
+                      "legacy": null
+                  }
+              }
+          ]
       },
       {
-        "id": "mbMT1",
-        "type": "mobile",
-        "subtype": "postpaid",
-        "label": {
-          "current": "Postpaid mobile",
-          "legacy": null
-        }
+          "id": "tES1",
+          "label": {
+              "current": "Etisalat",
+              "legacy": [null]
+          },
+          "type": "telecom",
+          "groupLabel": "Etisalat",
+          "opCom": true,
+          "opComLabel": "Etisalat UAE",
+          "prevScored": true,
+          "firstIndex": 2015,
+          "urlPreviousYearResults": "19wY05eGXMiOti59c7ZOmsILMDw-4gLWN4qGUtQtD7_U",
+          "tabPrevYearsOutcome": "EtisalatOutcome",
+          "urlCurrentDataCollectionSheet": null,
+          "urlCurrentCompanyScoringSheet": null,
+          "numberOfServices": 3,
+          "services": [{
+                  "id": "mbES1",
+                  "type": "mobile",
+                  "subtype": "prepaid",
+                  "label": {
+                      "current": "Prepaid mobile",
+                      "legacy": null
+                  }
+              },
+              {
+                  "id": "mbES2",
+                  "type": "mobile",
+                  "subtype": "postpaid",
+                  "label": {
+                      "current": "Postpaid mobile",
+                      "legacy": null
+                  }
+              },
+              {
+                  "id": "bbES1",
+                  "type": "broadband",
+                  "label": {
+                      "current": "Fixed-line broadband",
+                      "legacy": null
+                  }
+              }
+          ]
       },
       {
-        "id": "mbMT2",
-        "type": "mobile",
-        "subtype": "prepaid",
-        "label": {
-          "current": "Prepaid mobile",
-          "legacy": null
-        }
+          "id": "iFB1",
+          "label": {
+              "current": "Facebook",
+              "legacy": null
+          },
+          "type": "internet",
+          "groupLabel": "Facebook (Group)",
+          "opCom": false,
+          "opComLabel": null,
+          "prevScored": true,
+          "firstIndex": 2015,
+          "urlPreviousYearResults": "https://docs.google.com/spreadsheets/d/19wY05eGXMiOti59c7ZOmsILMDw-4gLWN4qGUtQtD7_U",
+          "tabPrevYearsOutcome": "FacebookOutcome",
+          "urlCurrentDataCollectionSheet": null,
+          "urlCurrentCompanyScoringSheet": null,
+          "numberOfServices": 4,
+          "services": [{
+                  "id": "snFB1",
+                  "type": "socialNetworkBlogs",
+                  "label": {
+                      "current": "Facebook",
+                      "legacy": null
+                  }
+              },
+              {
+                  "id": "pvFB1",
+                  "type": "photoVideo",
+                  "label": {
+                      "current": "Instagram",
+                      "legacy": null
+                  }
+              },
+              {
+                  "id": "mvFB1",
+                  "type": "messagingVoip",
+                  "label": {
+                      "current": "WhatsApp",
+                      "legacy": null
+                  }
+              },
+              {
+                  "id": "mvFB2",
+                  "type": "messagingVoip",
+                  "label": {
+                      "current": "Messenger",
+                      "legacy": null
+                  }
+              }
+          ]
+      },
+      {
+          "id": "tMT1",
+          "label": {
+              "current": "MTN",
+              "legacy": [null]
+          },
+          "type": "telecom",
+          "groupLabel": "MTN (Group)",
+          "opCom": true,
+          "opComLabel": "MTN South Africa (Operating Company)",
+          "prevScored": true,
+          "firstIndex": 2015,
+          "urlPreviousYearResults": "https://docs.google.com/spreadsheets/d/1mOB5yheHPKrPAb_HTpJA27IxligiggJHM4MCv1JGh9Y",
+          "tabPrevYearsOutcome": "MTNOutcome",
+          "urlCurrentDataCollectionSheet": null,
+          "urlCurrentCompanyScoringSheet": null,
+          "numberOfServices": 3,
+          "services": [{
+                  "id": "mbMT1",
+                  "type": "mobile",
+                  "subtype": "prepaid",
+                  "label": {
+                      "current": "Prepaid mobile",
+                      "legacy": null
+                  }
+              },
+              {
+                  "id": "mbMT2",
+                  "type": "mobile",
+                  "subtype": "postpaid",
+                  "label": {
+                      "current": "Postpaid mobile",
+                      "legacy": null
+                  }
+              },
+              {
+                  "id": "bbMT1",
+                  "type": "broadband",
+                  "label": {
+                      "current": "Fixed-line broadband",
+                      "legacy": null
+                  }
+              }
+          ]
+      },
+      {
+          "id": "iVM1",
+          "label": {
+              "current": "Verizon Media",
+              "legacy": [
+                  "Oath",
+                  "Yahoo"
+              ]
+          },
+          "type": "internet",
+          "groupLabel": "Verizon Media",
+          "opCom": false,
+          "opComLabel": null,
+          "prevScored": true,
+          "firstIndex": 2015,
+          "urlPreviousYearResults": "19wY05eGXMiOti59c7ZOmsILMDw-4gLWN4qGUtQtD7_U",
+          "tabPrevYearsOutcome": "OathOutcome",
+          "urlCurrentDataCollectionSheet": null,
+          "urlCurrentCompanyScoringSheet": null,
+          "numberOfServices": 2,
+          "services": [{
+                  "id": "maYM1",
+                  "type": "mail",
+                  "label": {
+                      "current": "Yahoo Mail",
+                      "legacy": null
+                  }
+              },
+              {
+                  "id": "pvTR1",
+                  "type": "photoVideo",
+                  "label": {
+                      "current": "Tumblr",
+                      "legacy": null
+                  }
+              }
+          ]
       }
-      ]
-    },
-    {
-      "id": "tES1",
-      "label": {
-        "current": "Etisalat",
-        "legacy": [null]
-      },
-      "type": "telecom",
-      "groupLabel": "Etisalat",
-      "opCom": true,
-      "opComLabel": "Etisalat UAE",
-      "prevScored": true,
-      "firstIndex": 2015,
-      "urlPreviousYearResults": "19wY05eGXMiOti59c7ZOmsILMDw-4gLWN4qGUtQtD7_U",
-      "tabPrevYearsOutcome": "EtisalatOutcome",
-      "urlCurrentDataCollectionSheet": null,
-      "urlCurrentCompanyScoringSheet": null,
-      "numberOfServices": 3,
-      "services": [{
-        "id": "bbES1",
-        "type": "broadband",
-        "label": {
-          "current": "Fixed-line broadband",
-          "legacy": null
-        }
-      },
-      {
-        "id": "mbES1",
-        "type": "mobile",
-        "subtype": "postpaid",
-        "label": {
-          "current": "Postpaid mobile",
-          "legacy": null
-        }
-      },
-      {
-        "id": "mbES2",
-        "type": "mobile",
-        "subtype": "prepaid",
-        "label": {
-          "current": "Prepaid mobile",
-          "legacy": null
-        }
-      }
-      ]
-    }
+
   ]
 }
