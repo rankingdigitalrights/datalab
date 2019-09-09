ef1950e02110f2413fb60a48cf7ac71c965f72df
fubits <ilja_s@pm.me>
2019-09-09 18:35:10 +0200

    Create 00_vectors.js
    For testing local JSON handling

---
+++ clasp-proj/00_vectors.js
@@ -0,0 +1,97 @@
+var companiesVector = {
+  "collection": "companies",
+  "indexYear": 2019,
+  "indexType": "RDR",
+  "companies": [
+    {
+      "id": "tMT1",
+      "label": {
+        "current": "MTN",
+        "legacy": [null]
+      },
+      "type": "telecom",
+      "groupLabel": "MTN (Group)",
+      "opCom": true,
+      "opComLabel": "MTN South Africa (Operating Company)",
+      "prevScored": true,
+      "firstIndex": 2015,
+      "urlPreviousYearResults": "https://docs.google.com/spreadsheets/d/1mOB5yheHPKrPAb_HTpJA27IxligiggJHM4MCv1JGh9Y",
+      "tabPrevYearsOutcome": "MTNOutcome",
+      "urlCurrentDataCollectionSheet": null,
+      "urlCurrentCompanyScoringSheet": null,
+      "numberOfServices": 3,
+      "services": [{
+        "id": "bbMT1",
+        "type": "broadband",
+        "label": {
+          "current": "Fixed-line broadband",
+          "legacy": null
+        }
+      },
+      {
+        "id": "mbMT1",
+        "type": "mobile",
+        "subtype": "postpaid",
+        "label": {
+          "current": "Postpaid mobile",
+          "legacy": null
+        }
+      },
+      {
+        "id": "mbMT2",
+        "type": "mobile",
+        "subtype": "prepaid",
+        "label": {
+          "current": "Prepaid mobile",
+          "legacy": null
+        }
+      }
+      ]
+    },
+    {
+      "id": "tES1",
+      "label": {
+        "current": "Etisalat",
+        "legacy": [null]
+      },
+      "type": "telecom",
+      "groupLabel": "Etisalat",
+      "opCom": true,
+      "opComLabel": "Etisalat UAE",
+      "prevScored": true,
+      "firstIndex": 2015,
+      "urlPreviousYearResults": "19wY05eGXMiOti59c7ZOmsILMDw-4gLWN4qGUtQtD7_U",
+      "tabPrevYearsOutcome": "EtisalatOutcome",
+      "urlCurrentDataCollectionSheet": null,
+      "urlCurrentCompanyScoringSheet": null,
+      "numberOfServices": 3,
+      "services": [{
+        "id": "bbES1",
+        "type": "broadband",
+        "label": {
+          "current": "Fixed-line broadband",
+          "legacy": null
+        }
+      },
+      {
+        "id": "mbES1",
+        "type": "mobile",
+        "subtype": "postpaid",
+        "label": {
+          "current": "Postpaid mobile",
+          "legacy": null
+        }
+      },
+      {
+        "id": "mbES2",
+        "type": "mobile",
+        "subtype": "prepaid",
+        "label": {
+          "current": "Prepaid mobile",
+          "legacy": null
+        }
+      }
+      ]
+    }
+  ]
+}
