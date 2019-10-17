b80373d48f18f70cefb1884cc18167204830b02d
gutermul <gutermuth@rankingdigitalrights.org>
2019-10-17 12:53:51 +0200

    Update 01_JSON_indicators.js

--- clasp-proj/01_JSON_indicators.js
+++ clasp-proj/01_JSON_indicators.js
@@ -18,77 +18,232 @@ var indicatorsVector = {
             // ],
             "indicators": [
                 {
-                    "labelShort": "G2",
-                    "labelLong": "Governance and management oversight",
+                    "labelShort": "G1",
+                    "labelLong": "Policy Commitments",
                     "description": "The company’s senior leadership should exercise oversight over how its policies and practices affect freedom of expression and privacy.",
                     "scoringScope": "full",
                     "y2yCompColumn": 2,
                     "y2yCompRow": 16,
                     "elements": [{
-                            "labelShort": "G2.1",
-                            "labelLogical": "G0201",
-                            "description": "Does the company clearly disclose that the board of directors exercises formal oversight over how company practices affect freedom of expression and privacy?"
-                        },
-                        {
-                            "labelShort": "G2.2",
-                            "description": "Does the company clearly disclose that an executive-level committee, team, program or officer oversees how company practices affect freedom of expression and privacy?"
-                        },
-                        {
-                            "labelShort": "G2.3",
-                            "description": "Does the company clearly disclose that a management-level committee, team, program or officer oversees how company practices affect freedom of expression and privacy?"
+                            "labelShort": "G1.3",
+                            "description": "Does the company disclose an explicit, clearly articulated policy document outlining their human rights commitments governing the development and use of algorithmic decision-making systems?"
                         }
                     ]
                 },
                 {
-                    "labelShort": "G4",
-                    "labelLong": "Impact assessment",
+                    "labelShort": "G4a",
+                    "labelLong": "Impact assessment: Government regulations",
                     "description": "The company should conduct regular, comprehensive, and credible due diligence, such as human rights impact assessments, to identify how all aspects of its business impact freedom of expression and privacy.",
                     "scoringScope": "full",
                     "y2yCompColumn": 2,
                     "y2yCompRow": 54,
                     "elements": [{
-                            "labelShort": "G4.1",
-                            "description": "As part of its decision-making, does the company consider how laws affect freedom of expression and privacy in jurisdictions where it operates?"
+                            "labelShort": "G4a.1",
+                            "description": "Does the company consider how laws affect freedom of expression and information in jurisdictions where it operates?"
                         },
                         {
-                            "labelShort": "G4.2",
-                            "description": "Does the company regularly assess freedom of expression and privacy risks associated with existing products and services?"
+                            "labelShort": "G4a.2",
+                            "description": "Does the company consider how laws affect privacy in jurisdictions where it operates?"
                         },
                         {
-                            "labelShort": "G4.3",
-                            "description": "Does the company assess freedom of expression and privacy risks associated with a new activity, including the launch and/or acquisition of new products, services, or companies or entry into new markets?"
+                            "labelShort": "G4a.3",
+                            "description": "Does the company assess freedom of expression and informationrisks associated with existing products and services?"
                         },
                         {
-                            "labelShort": "G4.4",
-                            "description": "Does the company assess freedom of expression and privacy risks associated with the processes and mechanisms used to enforce its terms of service (ToS)?"
+                            "labelShort": "G4a.4",
+                            "description": "Does the company assess privacy risks associated with existing products and services?"
                         },
                         {
-                            "labelShort": "G4.5",
-                            "description": "Does the company disclose that it assesses freedom of expression and privacy risks associated with its use of automated decision-making, such as through the use of algorithms and/or artificial intelligence?"
+                            "labelShort": "G4a.5",
+                            "description": "Does the company assess freedom of expression and information risks associated with a new activity, including the launch and/or acquisition of new products, services, or companies, or entry into new markets?"
                         },
                         {
-                            "labelShort": "G4.6",
-                            "description": "Does the company assess freedom of expression and privacy risks associated with its targeted advertising policies and practices?"
+                            "labelShort": "G4a.6",
+                            "description": "Does the company assess privacy risks associated with a new activity, including the launch and/or acquisition of new products, services, or companies, or entry into new markets?"
                         },
                         {
-                            "labelShort": "G4.7",
-                            "description": "Does the company conduct additional evaluation wherever the company’s risk assessments identify concerns?"
+                            "labelShort": "G4a.7",
+                            "description": "Does the company disclose that it conducts additional evaluation whenrever the company’s risk assessments identify concerns?"
                         },
                         {
-                            "labelShort": "G4.8",
-                            "description": "Do senior executivesand/or members of the company’s board of directors review and consider the results of assessments and due diligence in their decision-making?"
+                            "labelShort": "G4a.8",
+                            "description": "Does the company disclose that senior executives and/or members of the company’s board of directors review and consider the results of assessments and due diligence in their decision-making?"
                         },
                         {
-                            "labelShort": "G4.9",
+                            "labelShort": "G4a.9",
                             "description": "Does the company conduct assessments on a regular schedule?"
                         },
                         {
-                            "labelShort": "G4.10",
+                            "labelShort": "G4a.10",
                             "description": "Are the company’s assessments assured by an external third party?"
                         },
                         {
-                            "labelShort": "G4.11",
+                            "labelShort": "G4a.11",
                             "description": "Is the external third party that assures the assessment accredited to a relevant and reputable human rights standard by a credible organization?"
+                        }
+                    ]
+                },
+                {
+                    "labelShort": "G4b",
+                    "labelLong": "Impact assessment: Processes for company policy enforcement",
+                    "description": "The company should conduct regular, comprehensive, and credible due diligence, such as human rights impact assessments, to identify how all aspects of its business impact freedom of expression and privacy.",
+                    "scoringScope": "full",
+                    "y2yCompColumn": 2,
+                    "y2yCompRow": 54,
+                    "elements": [{
+                            "labelShort": "G4b.1",
+                            "description": "Does the company disclose that it assesses freedom of expression and information risks associated with its processes for enforcing its terms of service?"
+                        },
+                        {
+                            "labelShort": "G4b.2",
+                            "description": "Does the company assess how effectively it enforces its privacy policies?"
+                        },
+                        {
+                            "labelShort": "G4b.3",
+                            "description": "Does the company disclose that it assesses discrimination risks associated with its processes for enforcing its terms of service?"
+                        },
+                        {
+                            "labelShort": "G4b.4",
+                            "description": "Does the company disclose that it assesses discrimination risks associated with its processes for enforcing its privacy policies?"
+                        },
+                        {
+                            "labelShort": "G4b.5",
+                            "description": "Does the company conduct additional evaluation wherever the company’s risk assessments identify concerns?"
+                        },
+                        {
+                            "labelShort": "G4b.6",
+                            "description": "Do senior executives and/or members of the company’s board of directors review and consider the results of assessments and due diligence in their decision-making?"
+                        },
+                        {
+                            "labelShort": "G4b.7",
+                            "description": "Does the company conduct assessments on a regular schedule?"
+                        },
+                        {
+                            "labelShort": "G4b.8",
+                            "description": "Are the company’s assessments assured by an external third party?"
+                        },
+                        {
+                            "labelShort": "G4b.9",
+                            "description": "Is the external third party that assures the assessment accredited to a relevant and reputable human rights standard by a credible organization?"
+                        }
+                    ]
+                },
+                {
+                    "labelShort": "G4c",
+                    "labelLong": "Impact assessment: Targeted advertising",
+                    "description": "The company should conduct regular, comprehensive, and credible due diligence, such as human rights impact assessments, to identify how all aspects of its business impact freedom of expression and privacy.",
+                    "scoringScope": "full",
+                    "y2yCompColumn": 2,
+                    "y2yCompRow": 54,
+                    "elements": [{
+                            "labelShort": "G4c.1",
+                            "description": "Does the company disclose that it assesses freedom of expression and information risks associated with its targeted advertising policies and practices?"
+                        },
+                        {
+                            "labelShort": "G4c.2",
+                            "description": "Does the company disclose that it assesses privacy risks associated with its targeted advertising policies and practices?"
+                        },
+                        {
+                            "labelShort": "G4c.3",
+                            "description": "Does the company disclose that it assesses discrimination risks associated with its targeted advertising policies and practices?"
+                        },
+                        {
+                            "labelShort": "G4c.4",
+                            "description": "Does the company conduct additional evaluation wherever the company’s risk assessments identify concerns?"
+                        },
+                        {
+                            "labelShort": "G4c.5",
+                            "description": "Do senior executives and/or members of the company’s board of directors review and consider the results of assessments and due diligence in their decision-making?"
+                        },
+                        {
+                            "labelShort": "G4c.6",
+                            "description": "Does the company conduct assessments on a regular schedule?"
+                        },
+                        {
+                            "labelShort": "G4c.7",
+                            "description": "Are the company’s assessments assured by an external third party?"
+                        },
+                        {
+                            "labelShort": "G4c.8",
+                            "description": "Is the external third party that assures the assessment accredited to a relevant and reputable human rights standard by a credible organization?"
+                        }
+                    ]
+                },
+                {
+                    "labelShort": "G4d",
+                    "labelLong": "Impact assessment: Algorithmic decision-making systems",
+                    "description": "The company should conduct regular, comprehensive, and credible due diligence, such as human rights impact assessments, to identify how all aspects of its business impact freedom of expression and privacy.",
+                    "scoringScope": "full",
+                    "y2yCompColumn": 2,
+                    "y2yCompRow": 54,
+                    "elements": [{
+                            "labelShort": "G4d.1",
+                            "description": "Does the company disclose that it assesses freedom of expression and information risks associated with its development and use of algorithmic decision-making systems?"
+                        },
+                        {
+                            "labelShort": "G4d.2",
+                            "description": "Does the company disclose that it assesses privacy risks associated with its development and use of algorithmic decision-making systems?"
+                        },
+                        {
+                            "labelShort": "G4d.3",
+                            "description": "Does the company disclose that it assesses discrimination risks associated with its development and use of algorithmic decision making systems?"
+                        },
+                        {
+                            "labelShort": "G4d.4",
+                            "description": "Does the company conduct additional evaluation wherever the company’s risk assessments identify concerns?"
+                        },
+                        {
+                            "labelShort": "G4d.5",
+                            "description": "Do senior executives and/or members of the company’s board of directors review and consider the results of assessments and due diligence in their decision-making?"
+                        },
+                        {
+                            "labelShort": "G4d.6",
+                            "description": "Does the company conduct assessments on a regular schedule?"
+                        },
+                        {
+                            "labelShort": "G4d.7",
+                            "description": "Are the company’s assessments assured by an external third party?"
+                        },
+                        {
+                            "labelShort": "G4d.8",
+                            "description": "Is the external third party that assures the assessment accredited to a relevant and reputable human rights standard by a credible organization?"
+                        }
+                    ]
+                },
+                {
+                    "labelShort": "G6b",
+                    "labelLong": "Process for content moderation appeals",
+                    "description": "The company should conduct regular, comprehensive, and credible due diligence, such as human rights impact assessments, to identify how all aspects of its business impact freedom of expression and privacy.",
+                    "scoringScope": "full",
+                    "y2yCompColumn": 2,
+                    "y2yCompRow": 54,
+                    "elements": [{
+                            "labelShort": "G6b.1",
+                            "description": "Does the company disclose a policy that outlines its processes for offering and processing appeals of content moderation actions?"
+                        },
+                        {
+                            "labelShort": "G6b.2",
+                            "description": "Does the company clearly disclose that, when it takes a content moderation action, it immediately offers the affected user a chance to appeal the action?"
+                        },
+                        {
+                            "labelShort": "G6b.3",
+                            "description": "Does the company clearly disclose that such appeals are reviewed by at least one human not involved in the original content moderation action?"
+                        },
+                        {
+                            "labelShort": "G6b.4",
+                            "description": "Does the company clearly disclose that its appeals process gives the affected user an opportunity to present additional information that will be considered in the review?"
+                        },
+                        {
+                            "labelShort": "G6b.5",
+                            "description": "Does the company clearly disclose that, upon conclusion of an appeal, it provides the affected user a statement of the reasoning behind its decision?"
+                        },
+                        {
+                            "labelShort": "G6b.6",
+                            "description": "Does the company clearly disclose the timeframe within which it seeks to review appeals?"
+                        },
+                        {
+                            "labelShort": "G6b.7",
+                            "description": "Does the company clearly disclose whether there are any conditions under which its mechanism for appealing content moderation actions is not available?"
                         }
                     ]
                 }
