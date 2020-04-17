183afe24c18b46287f347cac407b019c0b1e19d0
fubits <ilja_s@pm.me>
2020-04-17 19:27:12 +0200

    V8 Broken: load external libraries | Issue

--- src/997_external_libraries_js.js
+++ src/997_external_libraries_js.js
@@ -1,16 +1,17 @@
 // Credit Brian @github
 // https://www.labnol.org/code/20380-load-external-javascript-with-eval
 
-var LIBRARIES = {
-    prettyDate: "http://ejohn.org/files/pretty.js",
-    underScore: "http://underscorejs.org/underscore-min.js",
-}
+// TODO: Migrate to V8
+// var LIBRARIES = {
+//     prettyDate: "http://ejohn.org/files/pretty.js",
+//     underScore: "http://underscorejs.org/underscore-min.js",
+// }
 
-Object.keys(LIBRARIES).forEach(function (library) {
-    newFunc = loadJSFromUrl(LIBRARIES[library])
-    eval("var " + library + " = " + newFunc)
-})
+// Object.keys(LIBRARIES).forEach(function (library) {
+//     newFunc = loadJSFromUrl(LIBRARIES[library])
+//     eval("var " + library + " = " + newFunc)
+// })
 
-function loadJSFromUrl(url) {
-    return eval(UrlFetchApp.fetch(url).getContentText())
-}
+// function loadJSFromUrl(url) {
+//     return eval(UrlFetchApp.fetch(url).getContentText())
+// }
