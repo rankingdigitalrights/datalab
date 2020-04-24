f0c95c9052ec19259752c22de8e7a66daab043aa
gabywalton <52406480+gabywalton@users.noreply.github.com>
2020-04-24 10:15:47 -0400

    Update Test.js

--- toys/Test.js
+++ toys/Test.js
@@ -6,5 +6,5 @@ function testSomething() {
 
     var testEffect = centralConfig.indexPrefix
     //  var testEffect = createSpreadsheet("2019 Pilot - Amazon DC Testing", false)
-    Logger.log(testEffect)
+    Logger.log(testEffect+"test run")
 }
