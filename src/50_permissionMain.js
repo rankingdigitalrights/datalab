69e265c619b982092d6db981fa3d0b4e2329f5f4
gabywalton <52406480+gabywalton@users.noreply.github.com>
2020-05-04 12:02:12 -0400

    Permission Functions Update
    Created two Permission functions, openStep still needs to be debugged though

--- src/50_permissionMain.js
+++ src/50_permissionMain.js
@@ -7,143 +7,157 @@ https://developers.google.com/apps-script/reference/spreadsheet/protection
 
 */
 
-
-function permissionsTesting() {
-    Logger.log("in permissionsTesting")
-
-  
-    var Sheet=SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1b0vYmNq51hb7_Nejd4RMpetIhN63A-iQvvI4_J5ZE9g/edit#gid=685545106');
-    var notation = Sheet.getRangeByName("RDR20DCundefinedG4biAP1Step").getA1Notation();
-      //var notation = Sheet.getRangeByName("RDR20DCundefinedG4biAP1Step").getRange();
-
-  var range = Sheet.getRange(notation);
-      
-  Logger.log(range)
-  
-  //var range = Sheet.getRange('B101:D121');
-    var protection = range.protect().setDescription('Sample protected range');
-
-    var me = Session.getEffectiveUser();
-    //protection.addEditor(me);
-
-    protection.removeEditors(protection.getEditors());
-    if (protection.canDomainEdit()) {protection.setDomainEdit(false);}
-
-
-
-}
-
-
-
-function openStep(IndicatorsObj, ResearchStepsObj, CompanyVec, StepVec, EmailVec){
-     // IndicatorsObj is the JSON containing all the indicator info
-    // ResearchStepsObj is the JSON containing all the indicator info
-    // CompanyVec is a vector of all the companies whose research steps will be opened
-    // StepVec is a vector of all the steps that will be opened to all
-    // Email Vec is a vector of all the emails whose permissions will be given
-
-    // INITIAL PSEUDOCODE
-
-
-    /*
-    Step 0: heal the document to make sure the named ranges are correct
-
-
-    Step 1: looping through the Company Vector
-    for (var i = 0; i < CompanyVec.length; i++) {
-       
-        Step 2: looping through Step Vector
-        for (var j=0; j<StepVec.length; j++){
-
-            Step 3: recreate the named range that applies to this step
-            use an already existing function????
-
-            Step 4: loop through email vector
-            for (var k=0; k<EmailVec; k++)
-
-                Step 5: give permissions for each of the emails listed
-
-
-        }
-       
-       
-    }
-
-    */
-}
-
-function closeStep(IndicatorsObj, ResearchStepsObj, CompanyVec, StepVec, EmailVec){
-    // IndicatorsObj is the JSON containing all the indicator info
-    // ResearchStepsObj is the JSON containing all the indicator info
-    // CompanyVec is a vector of all the companies whose research steps will be closed
-    // StepVec is a vector of all the steps that will be closed
-    // Email Vec is a vector of all the emails whose permissions will be rescided
-
-    // INITIAL PSEUDOCODE
-
-
-    /*
-    Step 0: heal the document to make sure the named ranges are correct
-
-
-    Step 1: looping through the Company Vector
-    for (var i = 0; i < CompanyVec.length; i++) {
-       
-        Step 2: looping through Step Vector
-        for (var j=0; j<StepVec.length; j++){
-
-            Step 3: recreate the named range that applies to this step
-            use an already existing function????
-
-            Step 4: loop through email vector
-            for (var k=0; k<EmailVec; k++)
-
-                Step 5: revoke permissions for each of the emails listed
-
-
-        }
-       
-       
-    }
-
-    */
+function openStep() {
+    // might want to call removeAll and then protect sheets to make sure all the permissions are correct?????
+    // future input vars: url
+        Logger.log("protectSheets")
+        
+    let Indicators = indicatorsVector
+    var StepLabelShort="subStepID" // make this a variable later
+    var emails = ["ggw12@georgetown.edu","sperling@rankingdigitalrights.org"]
     
-}
-
-function closeStepAll(IndicatorsObj, ResearchStepsObj, CompanyVec, StepVec ) {
-    // IndicatorsObj is the JSON containing all the indicator info
-    // ResearchStepsObj is the JSON containing all the indicator info
-    // CompanyVec is a vector of all the companies whose research steps will be closed
-    // StepVec is a vector of all the steps that will be closed to all
-
-
-    // INITIAL PSEUDOCODE
-
-
-    /*
-    Step 0: heal the document to make sure the named ranges are correct
-
-
-    Step 1: looping through the Company Vector
-    for (var i = 0; i < CompanyVec.length; i++) {
-       
-        Step 2: looping through Step Vector
-        for (var j=0; j<StepVec.length; j++){
-
-            Step 3: recreate the named range that applies to this step
-            use an already existing function????
-
-            Step 4: revoke permissions to that step
-
-
-        }
-       
-       
+    var Spread=SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1VAnqcoRUKdsrKWL1bPRRZiGKpLdapZ4aUGFYeI7iXh8/edit#gid=744690756');
+    
+    for (let i = 0; i < Indicators.indicatorCategories.length; i++) {
+  
+          var Category = Indicators.indicatorCategories[i]
+  
+          Logger.log("--- NEXT : Starting " + Category.labelLong)
+  
+  
+          // getting indicator individual sheets
+      for (var j = 0; j < Category.indicators.length; j++) {
+        var Indicator = Category.indicators[j]
+        var sheet=Spread.getSheetByName(Indicator.labelShort)
+        
+        Logger.log(sheet.getProtections(SpreadsheetApp.ProtectionType.SHEET))
+        var protections = sheet.getProtections(SpreadsheetApp.ProtectionType.SHEET);
+  for (var k = 0; k < protections.length; k++) {
+    if (protections[k].getDescription() == Indicator.labelShort) {
+      var protection=protections[k]
+      
+      // now need to build the namedRange you want, get A1 notation, then unprotect it, then protect it and open it only to certain people
+      
+      var namedR=defineNamedRangeStringImport("RDR20", "DC", StepLabelShort, Indicator.labelShort, component, "iAP1", "", "Step")    
+      var notation = Spread.getRangeByName(namedR).getA1Notation();
+      var range = sheet.getRange(notation); 
+      var rangeArray=[range]   
+      
+      protection.setUnprotectedRanges(rangeArray) // now this step is unprotected
+      
+      // create a new protection that will only be open to certain people
+      var protectionStep = range.protect().setDescription(StepLabelShort+"Protection");
+      
+      var me = Session.getEffectiveUser()
+      protectionStep.addEditor(me)
+  
+      protectionStep.removeEditors(protectionStep.getEditors());
+      if (protectionStep.canDomainEdit()) {protectionStep.setDomainEdit(false);}
+      
+          protection.addEditors(emails);
+              Spread.addEditors(emails);
+  
+      
+  
+      
+      
     }
-
-    */
-
-
-
-
+  
+  
+  
+  function removeAllProtections() {
+   Logger.log("In removeAllProtections")
+   var Spread=SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1VAnqcoRUKdsrKWL1bPRRZiGKpLdapZ4aUGFYeI7iXh8/edit#gid=744690756')
+    var sheets = Spread.getSheets()
+  for (var i = 0; i < sheets.length ; i++ ) {
+    var sheet = Spread.getSheets()[i]
+    
+     Logger.log("In "+sheet)
+  
+    
+    var protections=sheet.getProtections(SpreadsheetApp.ProtectionType.RANGE)
+    var protect=sheet.getProtections(SpreadsheetApp.ProtectionType.SHEET)
+    for (var j = 0; j < protections.length; j++) {
+      protections[j].remove()
+    }
+    for (var j = 0; j < protect.length; j++) {
+      protect[j].remove()
+    }
+    
+    }
+    
+  }
+  
+  function protectSheets() {
+    // future input vars: url
+        Logger.log("protectSheets")
+        
+    let Indicators = indicatorsVector;
+    
+        var Spread=SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1VAnqcoRUKdsrKWL1bPRRZiGKpLdapZ4aUGFYeI7iXh8/edit#gid=744690756');
+    
+    for (let i = 0; i < Indicators.indicatorCategories.length; i++) {
+  
+          var Category = Indicators.indicatorCategories[i]
+  
+          Logger.log("--- NEXT : Starting " + Category.labelLong)
+  
+  
+          // protecting individual sheets
+      for (var j = 0; j < Category.indicators.length; j++) {
+  
+          var Indicator = Category.indicators[j]
+           var protection=Spread.getSheetByName(Indicator.labelShort).protect().setDescription(Indicator.labelShort)
+        
+        var me = Session.getEffectiveUser();
+      protection.addEditor(me);
+  
+      protection.removeEditors(protection.getEditors());
+      if (protection.canDomainEdit()) {protection.setDomainEdit(false);}
+  
+          
+          
+          Logger.log("indicator :" + Indicator.labelShort)
+      }
+          
+          
+          Logger.log("--- Completed " + Category.labelLong)
+          
+          
+      }
+  
+  
+    
+  }
+  
+  function permissionsTesting() {
+      Logger.log("in permissionsTesting")
+      
+      var Spread=SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1VAnqcoRUKdsrKWL1bPRRZiGKpLdapZ4aUGFYeI7iXh8/edit#gid=744690756');
+  
+      //var Sheet=SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1b0vYmNq51hb7_Nejd4RMpetIhN63A-iQvvI4_J5ZE9g/edit#gid=685545106');
+      //var notation = Sheet.getRangeByName("RDR20DCundefinedG4biAP1Step").getA1Notation();
+        //var notation = Sheet.getRangeByName("RDR20DCundefinedG4biAP1Step").getRange();
+  
+    //var range = Sheet.getRange(notation);
+        
+    //Logger.log(range)
+    
+    //var range = Sheet.getRange('B101:D121');
+     // var protection = range.protect().setDescription('Sample protected range');
+    
+    var protection=Spread.getSheets()[2].protect().setDescription('Sample protected range');
+  
+      var me = Session.getEffectiveUser();
+      //protection.addEditor(me);
+  
+      protection.removeEditors(protection.getEditors());
+    protection.addEditor('ggw12@georgetown.edu');
+      if (protection.canDomainEdit()) {protection.setDomainEdit(false);}
+    
+        let Indicators = indicatorsVector;
+  
+  
+  
+  }
 }
