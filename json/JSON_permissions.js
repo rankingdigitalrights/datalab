1bc3f320cc8dd3aeae9d5f12dd1bae3adcbf05ea
gabywalton <52406480+gabywalton@users.noreply.github.com>
2020-05-18 10:52:25 -0400

    Create JSON_permissions.js
    This is a brief outline of a potential json for the permissions function

---
+++ json/JSON_permissions.js
@@ -0,0 +1,16 @@
+var permissionsVector={
+    companies:[{
+        label:"Apple",
+        id: "iAP1",
+        urlCurrentDataCollectionSheet:"1S3FeoyLdyJHkP3NKGkQf99d6xVl3Ao1dUtPHT6JOGBY",
+        sheetAccessEmails:["walton@rankingdigitalrights.org"],
+        steps:[{
+            step:1,
+            shortLabels:["S010","S011","S015"],
+            stepAccessEmails:["walton@rankingdigitalrights.org"]
+        }]
+
+    }]
+
+
+}
