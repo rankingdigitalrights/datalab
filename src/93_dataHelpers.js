95b7111d4b0ee54db7337d7dd702183f6deb22b7
gabywalton <52406480+gabywalton@users.noreply.github.com>
2020-06-23 12:02:58 -0400

    Code to create percentage done formula added

--- src/93_dataHelpers.js
+++ src/93_dataHelpers.js
@@ -150,3 +150,99 @@ function createSubstepArray(stepLabel) {
 
     return stepArray
 }
+
+
+function createFormula(){
+    //let Company=companiesVector.companies.slice(0,1)[0]
+    let stepLabel="S020"
+    
+    let formula=""
+    let SS=SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/1Etb_IxD2Xl_bITrbAw7mWepZjQu3N8KSuuyAGEG_MTM/edit#gid=0")
+    let Sheet=SS.getActiveSheet()
+    let row=1
+    
+    
+    let Companies = companiesVector.companies
+          // .slice(0, 0) // on purpose to prevent script from running.
+          // .slice(0, 1) //   0 "Alibaba",
+          // .slice(1, 2) //   1 "Amazon",
+          // .slice(2, 3) //   2 "América Móvil",
+          // .slice(3, 4) //   3 "Apple",
+          // .slice(4, 5) //   4 "AT&T",
+          // .slice(5, 6) //   5 "Axiata",
+          // .slice(6, 7) //   6 "Baidu",
+          // .slice(7, 8) //   7 "Bharti Airtel",
+          // .slice(8, 9) //   8 "Deutsche Telekom",
+          // .slice(9, 10) //   9 "Etisalat",
+          //.slice(10, 11) //   10 "Facebook",
+      // .slice(11, 12) //   11 "Google",
+      // .slice(12, 13) //   12 "Kakao",
+      // .slice(13, 14) //   13 "Mail.Ru",
+      // .slice(14, 15) //   14 "Microsoft",
+      // .slice(15, 16) //   15 "MTN",
+      // .slice(16, 17) //   16 "Ooredoo",
+      // .slice(17, 18) //   17 "Orange",
+      // .slice(18, 19) //   18 "Samsung",
+      // .slice(19, 20) //   19 "Telefónica",
+      // .slice(20, 21) //   20 "Telenor",
+      // .slice(21, 22) //   21 "Tencent",
+      // .slice(22, 23) //   22 "Twitter",
+      // .slice(23, 24) //   23 "Verizon Media",
+      // .slice(24, 25) //   24 "Vodafone",
+      // .slice(25, 26) //   25 "Yandex"
+  
+      Companies.forEach(function (Company) {
+          formula=createPercentageDone(Company,stepLabel)
+          range="A"+String(row)
+          Sheet.getRange(range).setFormula(formula)
+          row=row+1
+      })
+    
+    
+    
+    
+  }
+  
+  function createPercentageDone(Company,stepLabel) {
+      let rangeName, Indicator
+  
+      let finalFormula=""
+      let formulaNotSelected="=1-("
+      let formulaAllCells="/("
+      //let Indicators = subsetIndicatorsObject(indicatorsVector, "G1|G2")
+      let Indicators = indicatorsVector
+      let currentPrefix = centralConfig.indexPrefix
+      
+      Logger.log("indicator:"+Indicators)
+  
+      // looping through the types of indicators
+      for (let indicatorCategory = 0; indicatorCategory < Indicators.indicatorCategories.length; indicatorCategory++) {
+  
+          let Category = Indicators.indicatorCategories[indicatorCategory]
+  
+          Logger.log("--- Starting " + Category.labelLong)
+  
+          // looping through each indicator
+          for (let indicator = 0; indicator < Category.indicators.length; indicator++) {
+              Indicator = Category.indicators[indicator]
+              rangeName = defineNamedRange(currentPrefix, "DC", stepLabel, Indicator.labelShort, "", Company.id, "", "Step")
+              formulaNotSelected=formulaNotSelected+'COUNTIF(IMPORTRANGE("'+Company.urlCurrentDataCollectionSheet+'","'+rangeName+'"),"*not selected")+'
+              formulaAllCells=formulaAllCells+'COUNTIF(IMPORTRANGE("'+Company.urlCurrentDataCollectionSheet+'","'+rangeName+'"),"<>N/A")+'
+  
+    
+            
+          }
+  
+  
+      }
+  
+      formulaNotSelected=formulaNotSelected+'0)'
+      formulaAllCells=formulaAllCells+'0)'
+      
+      Logger.log("unselected:"+formulaNotSelected)
+      Logger.log("all:"+formulaAllCells)
+  
+      //return formulaAllCells
+      return formulaNotSelected+formulaAllCells
+  
+  }
