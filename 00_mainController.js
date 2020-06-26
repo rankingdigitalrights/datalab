97dace3dbc48504d547dd729aab68049f1252753
fubits <ilja_s@protonmail.com>
2020-06-26 19:14:41 +0200

    Controller: addNewStep moved to top

--- 00_mainController.js
+++ 00_mainController.js
@@ -124,6 +124,64 @@ function mainInputSheets() {
 
 }
 
+// --- // add a new Main Research Step at position Sheet.lastRow()
+function mainAddNewInputStep() {
+
+    initiateGlobalConfig()
+
+    updateProduction = false // IMPORTANT flag; if true then Company DC Sheet is grabbed by sheetID
+
+    addNewStep = true // Just ignore: also caution - doesn't care if step already exists
+    // also: Hook to skip steps
+    startAtMainStepNr = addNewStep ? 5 : 0 // logical Order
+
+    outputFolderName = isProduction ? Config.inputFolderNameProd : Config.inputFolderNameDev
+    // filenameSuffix = "" // local override : Dev, "", Debug, QC
+    let mainSheetMode = "Input" // for filename | TODO: move to Config
+    let useStepsSubset = true // true := use subset; maxStep defined in Config.JSON
+    let useIndicatorSubset = globalIndicatorsSubset // true := use subset
+
+    const Companies = companiesVector.companies
+        // .slice(0, 0) // on purpose to prevent script from running.
+        // .slice(0, 1) //   0 "Alibaba",
+        // .slice(1, 2) //   1 "Amazon",
+        .slice(2, 3) //   2 "América Móvil",
+    // .slice(3, 4) //   3 "Apple",
+    // .slice(4, 5) //   4 "AT&T",
+    // .slice(5, 6) //   5 "Axiata",
+    // .slice(6, 7) //   6 "Baidu",
+    // .slice(7, 8) //   7 "Bharti Airtel",
+    // .slice(8, 9) //   8 "Deutsche Telekom",
+    // .slice(9, 10) //   9 "Etisalat",
+    // .slice(10, 11) //   10 "Facebook",
+    // .slice(11, 12) //   11 "Google",
+    // .slice(12, 13) //   12 "Kakao",
+    // .slice(13, 14) //   13 "Mail.Ru",
+    // .slice(14, 15) //   14 "Microsoft",
+    // .slice(15, 16) //   15 "MTN",
+    // .slice(16, 17) //   16 "Ooredoo",
+    // .slice(17, 18) //   17 "Orange",
+    // .slice(18, 19) //   18 "Samsung",
+    // .slice(19, 20) //   19 "Telefónica",
+    // .slice(20, 21) //   20 "Telenor",
+    // .slice(21, 22) //   21 "Tencent",
+    // .slice(22, 23) //   22 "Twitter",
+    // .slice(23, 24) //   23 "Verizon Media",
+    // .slice(24, 25) //   24 "Vodafone",
+    // .slice(25, 26) //   25 "Yandex"
+
+    let fileID
+
+    Companies.forEach(function (Company) {
+
+        fileID = processInputSpreadsheet(useStepsSubset, useIndicatorSubset, Company, filenamePrefix, filenameSuffix, mainSheetMode)
+
+        addFileIDtoControl(mainSheetMode, Company.label.current, fileID, controlSpreadsheetID)
+
+    })
+
+}
+
 // create Scoring spreadsheets for all companies
 
 function mainScoringSheets() {
@@ -282,8 +340,8 @@ function mainRepairInputSheets() {
         // .slice(0, 0) // on purpose to prevent script from running.
         // .slice(0, 1) //   0 "Alibaba",
         // .slice(1, 2) //   1 "Amazon",
-        // .slice(2, 3) //   2 "América Móvil",
-        .slice(3, 4) //   3 "Apple",
+        .slice(2, 3) //   2 "América Móvil",
+    // .slice(3, 4) //   3 "Apple",
     // .slice(4, 5) //   4 "AT&T",
     // .slice(5, 6) //   5 "Axiata",
     // .slice(6, 7) //   6 "Baidu",
@@ -315,63 +373,6 @@ function mainRepairInputSheets() {
 
     })
 
-
-}
-
-function mainAddNewInputStep() {
-
-    initiateGlobalConfig()
-
-    updateProduction = false // IMPORTANT flag; ensures that Company DC Sheet is grabbed by sheetID
-
-    addNewStep = true // Caution: doesn't care if step already exists
-    // also: Hook to skip steps
-    startAtMainStepNr = addNewStep ? 3 : 0 // logical Order
-
-    outputFolderName = isProduction ? Config.inputFolderNameProd : Config.inputFolderNameDev
-    // filenameSuffix = "" // local override : Dev, "", Debug, QC
-    let mainSheetMode = "Input" // for filename | TODO: move to Config
-    let useStepsSubset = true // true := use subset; maxStep defined in Config.JSON
-    let useIndicatorSubset = globalIndicatorsSubset // true := use subset
-
-    const Companies = companiesVector.companies
-        // .slice(0, 0) // on purpose to prevent script from running.
-        // .slice(0, 1) //   0 "Alibaba",
-        // .slice(1, 2) //   1 "Amazon",
-        .slice(2, 3) //   2 "América Móvil",
-    // .slice(3, 4) //   3 "Apple",
-    // .slice(4, 5) //   4 "AT&T",
-    // .slice(5, 6) //   5 "Axiata",
-    // .slice(6, 7) //   6 "Baidu",
-    // .slice(7, 8) //   7 "Bharti Airtel",
-    // .slice(8, 9) //   8 "Deutsche Telekom",
-    // .slice(9, 10) //   9 "Etisalat",
-    // .slice(10, 11) //   10 "Facebook",
-    // .slice(11, 12) //   11 "Google",
-    // .slice(12, 13) //   12 "Kakao",
-    // .slice(13, 14) //   13 "Mail.Ru",
-    // .slice(14, 15) //   14 "Microsoft",
-    // .slice(15, 16) //   15 "MTN",
-    // .slice(16, 17) //   16 "Ooredoo",
-    // .slice(17, 18) //   17 "Orange",
-    // .slice(18, 19) //   18 "Samsung",
-    // .slice(19, 20) //   19 "Telefónica",
-    // .slice(20, 21) //   20 "Telenor",
-    // .slice(21, 22) //   21 "Tencent",
-    // .slice(22, 23) //   22 "Twitter",
-    // .slice(23, 24) //   23 "Verizon Media",
-    // .slice(24, 25) //   24 "Vodafone",
-    // .slice(25, 26) //   25 "Yandex"
-
-    let fileID
-
-    Companies.forEach(function (Company) {
-
-        fileID = processInputSpreadsheet(useStepsSubset, useIndicatorSubset, Company, filenamePrefix, filenameSuffix, mainSheetMode)
-
-        addFileIDtoControl(mainSheetMode, Company.label.current, fileID, controlSpreadsheetID)
-
-    })
 
 }
 
