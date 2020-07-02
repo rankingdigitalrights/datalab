85a2f3885d6e189cde171dcc304a10684ec64eac
gabywalton <52406480+gabywalton@users.noreply.github.com>
2020-07-02 10:53:33 -0400

    Update 51_permissionMainProtect.js

--- src/51_permissionMainProtect.js
+++ src/51_permissionMainProtect.js
@@ -24,18 +24,3 @@ function mainProtectSingleCompany(company) {
     protectSingleCompany(Indicators, Editors, SS, companyID, currentPrefix)
 
 }
-
-function mainUnProtectSingleCompany(company) {
-
-    let Company = company
-
-    let companyID = Company.id
-    Logger.log("CompanyObj :" + companyID)
-
-    let fileID = Company.urlCurrentDataCollectionSheet
-    let SS = SpreadsheetApp.openById(fileID) //<---------------- undo when we want to edit actual sheets
-    //let SS=SpreadsheetApp.openById("1u3F4xtzd89aVhO1UuWoNR_lPCFLsVXaom_xcDij5oKE")
-
-    // close step
-    removeAllProtections(SS)
-}
