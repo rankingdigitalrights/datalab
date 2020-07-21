4ce830d576d0d91635f1ab30fe7fc5e24e5cdbad
fubits <ilja_s@protonmail.com>
2020-07-21 17:36:44 +0200

    Update valueFinder.js

--- utils/valueFinder.js
+++ utils/valueFinder.js
@@ -1,40 +1,44 @@
 function mainDeleteStepRows() {
 
-    let stepString = "^Step 3"
+    let stepString = "^[G|F|P].*\n+Response" // "^Step 3"
+    let columnNr = 1 // starts with 0 := A so 1 := B
 
-    let SS = openSpreadsheetByID("1phB7j-fh3WZUeeEBAlqB7e0r5XI5N6pzm-PP3UlrqYY")
+    let SS = openSpreadsheetByID("1rsjAvw06vWjWZx3A1bptRpH8RL0-Ax-SfM3Zny4j_zY")
 
     // let Indicators = subsetIndicatorsObject(indicatorsVector, "G1") // F5a|P1$// indicatorsVector
     let Indicators = indicatorsVector
 
+    let result = []
     let Sheet, rows
     Indicators.indicatorCategories.forEach(Category =>
         Category.indicators.forEach(Indicator => {
             console.log(Indicator.labelShort)
             Sheet = SS.getSheetByName(Indicator.labelShort)
             if (Sheet !== null) {
-                rows = findStepRows(Sheet, stepString)
-                deleteRows(Sheet, rows[0], rows[1])
+                rows = findStepRows(Sheet, stepString, columnNr)
+                // deleteRows(Sheet, rows[0], rows[1]) // DA-A-A-NGER
+                result.push([Indicator.labelShort, rows[0]])
             } else {
                 console.log("Sheet for " + Indicator.labelShort + " not found")
             }
         }))
 
+    console.log(result)
 }
 
-function findStepRows(Sheet, stepString) {
+function findStepRows(Sheet, stepString, columnNr) {
     console.log("starting search")
     let raw = Sheet.getDataRange()
     let data = raw.getValues()
 
     let startRow = data.findIndex(r => {
-        if (r[1] !== null && r[1].length > 0) {
-            return r[1].match(stepString) // 1 := second column
+        if (r[columnNr] !== null && r[columnNr].length > 0) {
+            return r[columnNr].match(stepString) // 1 := second column
         }
     })
 
     let lastRow = Sheet.getLastRow()
-    console.log(`Range found: ${startRow} : ${lastRow}`)
+    // console.log(`Range found: ${startRow} : ${lastRow}`)
     return [startRow + 1, lastRow + 1]
 }
 
