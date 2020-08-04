e013fff393f6ed8e764d45bfcd2b547daad13018
fubits <ilja_s@protonmail.com>
2020-08-04 12:01:44 +0200

    Update print-envID.sh

--- env/print-envID.sh
+++ env/print-envID.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+echo "available projects are:\n"
+echo "datalab:" $(cat env/prodID.txt)
+echo "GW:" $(cat env/devID-gw.txt)
+echo "IS:" $(cat env/devID-is.txt)
+
 while read -r line; do
 # Reading each line
 id=$line
-echo "ENV Script Project ID:"
+echo "\nCurrent Project ID is:"
 echo "$id"
 done < .clasp.json
