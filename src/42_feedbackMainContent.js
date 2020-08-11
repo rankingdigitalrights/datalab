1ee5b2548cdcb758e64a41c78289cf24c001c667
fubits <ilja_s@pm.me>
2020-08-11 22:51:17 +0200

    IMPORTANT BUGFIX: columnToLetter redundancy unrolled and refactored

--- src/42_feedbackMainContent.js
+++ src/42_feedbackMainContent.js
@@ -253,13 +253,3 @@ function addFBExtraInstruction(Sheet, Indicator, activeRow, offsetCol, layoutWid
 
     return activeRow + 2
 }
-
-function columnToLetter(column) {
-    let temp, letter = ""
-    while (column > 0) {
-        temp = (column - 1) % 26
-        letter = String.fromCharCode(temp + 65) + letter
-        column = (column - temp - 1) / 26
-    }
-    return letter
-}
