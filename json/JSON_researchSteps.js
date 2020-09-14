29768ee270c3d9aedf3c409150ca60c9484e2ce5
fubits <ilja_s@protonmail.com>
2020-09-14 12:37:08 +0200

    Revert "Update JSON_researchSteps.js"
    This reverts commit 3d310152f86652b01af95b7da1c083dfeec2fec6.

--- json/JSON_researchSteps.js
+++ json/JSON_researchSteps.js
@@ -517,7 +517,7 @@ var researchStepsVector = {
         {
           rowLabel: "Research Team Notes",
           labelShort: "Step 4.1",
-          subStepID: "S041",
+          subStepID: "S042",
           subStepColor: "#ffe599",
           doCollapse: false,
           components: [{
