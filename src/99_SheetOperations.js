1ba9f928d374e235fd67ccfa80c7be8add298853
fubits <ilja_s@protonmail.com>
2020-10-13 19:56:48 +0200

    HELPER: crop Column Modified

--- src/99_SheetOperations.js
+++ src/99_SheetOperations.js
@@ -1,10 +1,11 @@
-function cropEmptyColumns(Sheet, offSet) {
+function cropEmptyColumns(Sheet, offSet, fixedLastColumn) {
 
-    offSet = offSet || 0
+    offSet = offSet || 0 // default value
+    fixedLastColumn = fixedLastColumn > 0 ? fixedLastColumn : 0
 
     let maxColumns = Sheet.getMaxColumns()
-    let lastColumn = Sheet.getLastColumn() + offSet
-    if (maxColumns - lastColumn != 0) {
+    let lastColumn = fixedLastColumn > 0 ? fixedLastColumn + offSet : Sheet.getLastColumn() + offSet
+    if (maxColumns - lastColumn !== 0) {
         Sheet.deleteColumns(lastColumn + 1, maxColumns - lastColumn)
     }
 }
