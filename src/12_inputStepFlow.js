e7b8cb77430d1b657165ab963f37be501be0e7fd
fubits <ilja_s@pm.me>
2020-10-20 10:20:20 +0200

    Revert "UNDO: Step 6 repairs hack"
    This reverts commit 8efee0daa203a3cbbc5884b059e8b0b98514c376.

--- src/12_inputStepFlow.js
+++ src/12_inputStepFlow.js
@@ -240,10 +240,9 @@ function addCommentsReview(SS, Sheet, Indicator, Company, activeRow, mainStepNr,
                 cellValue = `=IF(${reviewCell}="${conditional}",${prevResultCell},"${yesAnswer}")`
             }
 
-            // TODO: undo
-            // if (!doRepairsOnly) {
-            Cell.setValue(cellValue)
-            // }
+            if (!doRepairsOnly) {
+                Cell.setValue(cellValue)
+            }
 
             SS.setNamedRange(cellID, Cell) // names cells
 
@@ -330,10 +329,9 @@ function addSourcesReview(SS, Sheet, Indicator, Company, activeRow, Substep, ste
 
         cellValue = `=IF(AND(${reviewFormula}),${prevResultCell},"${yesAnswer}")`
 
-        // TODO: undo
-        // if (!doRepairsOnly) {
-        Cell.setValue(cellValue)
-        // }
+        if (!doRepairsOnly) {
+            Cell.setValue(cellValue)
+        }
 
         SS.setNamedRange(cellID, Cell) // names cells
 
